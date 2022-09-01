64db900a87d0da5575d983da4f47df84ec287845
adcoulibaly <coulibalyadamabekaye03@gmail.com>
2022-09-01 14:43:13 +0000

    mon test

--- src/main/java/com/syadama/APIErrorNote/Controller/SolutionController.java
+++ src/main/java/com/syadama/APIErrorNote/Controller/SolutionController.java
@@ -6,6 +6,8 @@ import com.syadama.APIErrorNote.Service.ProblemeService;
 import com.syadama.APIErrorNote.Service.SolutionService;
 import com.syadama.APIErrorNote.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,10 +22,12 @@ public class SolutionController {
     private ProblemeService problemeService;
     @Autowired
     UserRepository userRepository;
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-
-    @PostMapping("/ajouter/{email}/{titre}")
-    public String Ajouter(@RequestBody Solution solution,@PathVariable String email, @PathVariable String titre){
+    @PostMapping("/ajouter/{email}/{password}/{titre}")
+    public String Ajouter(@RequestBody Solution solution,@PathVariable String email,@PathVariable String password, @PathVariable String titre){
 
         User user = userRepository.findByEmail(email);
         Probleme probleme = problemeService.trouverProblemeParTitre(titre);
@@ -39,19 +43,26 @@ public class SolutionController {
                 if (IdUserPost != IdUser){
                     return "Ce probleme n'est pas a vous !";
                 }else {
-                    solution.setProbleme(probleme);
-                    Etat etat = new Etat();
+                    if (passwordEncoder().matches(password,user.getPassword()))
+                    {
+                        solution.setProbleme(probleme);
+                        Etat etat = new Etat();
 
-                    etat.setId_etat(2L);
+                        etat.setId_etat(2L);
 
-                    solution.getProbleme().setEtat(etat);
+                        solution.getProbleme().setEtat(etat);
 
-                    solutionService.ajouter(solution);
-                    return "Solutionné avec succès";
+                        solutionService.ajouter(solution);
+                        return "Solutionné avec succès ";
+                    }
+                    else{
+                        return "Mot de passe incorrect";
+                    }
+
                 }
             }
             else {
-                return "Ce probleme a été deja solutionné";
+                return "Ce probleme a été deja solutionné par"+user.getNom();
             }
 
         }else {
@@ -59,9 +70,23 @@ public class SolutionController {
         }
     }
 
-    @PutMapping("/modifier/{id_solution}")
-    public Solution modifer(@PathVariable Long id_solution,@RequestBody Solution solution){
-        return solutionService.modifier(id_solution,solution);
+    @PutMapping("/modifier/{email}/{password}/{id_solution}")
+    public String modifer(@PathVariable("email") String email,@PathVariable("password") String password,@PathVariable Long id_solution,@RequestBody Solution solution){
+
+        User user = userRepository.findByEmail(email);
+
+        if (user != null){
+            if (passwordEncoder().matches(password,user.getPassword())){
+                solutionService.modifier(id_solution,solution);
+                return "Solution modifier avec succès";
+            }
+            else {
+                return "Mot de passe incorrect";
+            }
+
+        }
+
+            return "Utilisateur intouvable";
     }
 
     @DeleteMapping("/supprimer/{id_solution}")
